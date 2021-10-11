66a0cf204caa81436c56368d651e42aa1332b670
paulErdos <vincesteffens@gmail.com>
2021-10-11 05:44:22 -0700

    correction: add qqq to qqq table

--- data_insert.sql
+++ data_insert.sql
@@ -10,4 +10,4 @@ BULK INSERT spy FROM '/Historical_Data/SPY.csv' with (format = 'CSV', firstrow=2
 
 create table qqq (date varchar(255), open_price float, high_price float, low_price float, close_price float, adj_close_price float, volume float, )
 
-BULK INSERT spy FROM '/Historical_Data/QQQ.csv' with (format = 'CSV', firstrow=2, fieldterminator = ',', rowterminator = '\n')
+BULK INSERT qqq FROM '/Historical_Data/QQQ.csv' with (format = 'CSV', firstrow=2, fieldterminator = ',', rowterminator = '\n')
