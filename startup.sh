12c019161272ae7d469a7f3ac27fe87db4f2f5d1
paulErdos <vincesteffens@gmail.com>
2021-10-12 10:50:25 -0700

    Setup database from startup script

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 #sudo docker pull mcr.microsoft.com/mssql/server:2019-latest
 docker stop sql1 && docker rm sql1
 
@@ -6,5 +8,7 @@ docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Aa345678" \
    -d mcr.microsoft.com/mssql/server:2019-latest
 
 docker cp Historical_Data/ sql1:/
-docker cp connect.sh sql1:/
+docker cp connect_and_setup.sh sql1:/
 docker cp data_insert.sql sql1:/
+
+docker exec sql1 bash connect_and_setup.sh
